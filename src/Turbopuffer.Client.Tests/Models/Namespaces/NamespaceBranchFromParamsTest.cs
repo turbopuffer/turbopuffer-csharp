0a3eeb76deefa1301ccc9571ff3e6fbe2d32cfc2
Nikhil Benesch <nikhil.benesch@gmail.com>
2026-05-18 12:41:56 -0400

    Remove BranchFrom body-content test

--- src/Turbopuffer.Client.Tests/Models/Namespaces/NamespaceBranchFromParamsTest.cs
+++ src/Turbopuffer.Client.Tests/Models/Namespaces/NamespaceBranchFromParamsTest.cs
@@ -1,33 +1,10 @@
 using System;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Turbopuffer.Client.Models.Namespaces;
 
 namespace Turbopuffer.Client.Tests.Models.Namespaces;
 
 public class NamespaceBranchFromParamsTest : TestBase
 {
-    [Fact]
-    public async Task BodyContent_WrapsInBranchFromNamespace()
-    {
-        NamespaceBranchFromParams parameters = new()
-        {
-            Namespace = "namespace",
-            SourceNamespace = "source_namespace",
-        };
-
-#pragma warning disable xUnit1051 // net472 has no CancellationToken overload of ReadAsStringAsync
-        var body = await parameters.BodyContent()!.ReadAsStringAsync();
-#pragma warning restore xUnit1051
-        using var document = JsonDocument.Parse(body);
-
-        var wrapped = document.RootElement.GetProperty("branch_from_namespace");
-        Assert.Equal(
-            "source_namespace",
-            wrapped.GetProperty("source_namespace").GetString()
-        );
-    }
-
     [Fact]
     public void FieldRoundtrip_Works()
     {
